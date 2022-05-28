9db70b0a03b9f9ae6960c3227af245a3d0cea9a8
gustavo viana <gustavovianaveiga721@gmail.com>
2022-05-28 11:32:23 -0300

    first commit

---
+++ src/components/Principal.vue
@@ -0,0 +1,60 @@
+<script setup>
+import '../bootstrap.min.css'
+import '../animate.min.css'
+import '../Principal.css'
+</script>
+
+<template>
+
+<div class="tamanho">
+
+   <header class="p-3 bg-dark text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+          <li><a href="#" class="nav-link px-2 text-white navlinkcolor">Features</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+        </ul>
+
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+          <input type="search" class="form-control form-control-dark text-white bg-dark" placeholder="Search..." aria-label="Search">
+        </form>
+
+        <div class="text-end">
+          <button type="button" class="btn btn-outline-light me-2">Login</button>
+          <button type="button" class="btn btn-outline-light">Sign-up</button>
+        </div>
+      </div>
+    </div>
+  </header>
+<main><h1>hello world</h1></main>
+
+<div class="container rodape">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+      </a>
+      <span class="mb-3 mb-md-0 text-muted">&copy; 2022 Company, Inc</span>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" class="githubicon" 
+      alt="">
+      <img src="https://iconape.com/wp-content/files/yd/367773/svg/logo-linkedin-logo-icon-png-svg.png" class="githubicon" alt="">
+    </ul>
+  </footer>
+</div>
+
+</div>
+
+</template>
+
+<style scoped>
+</style>
