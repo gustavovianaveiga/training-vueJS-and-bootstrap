7954984ef0921a85d20c3bc152967bbe46dd54d6
gustavo viana <gustavovianaveiga721@gmail.com>
2022-05-29 08:53:48 -0300

    utilizando componente card

---
+++ src/components/card.vue
@@ -0,0 +1,18 @@
+<script setup>
+import '../bootstrap.min.css'
+import '../animate.min.css'
+
+</script>
+
+<template>
+<div class="card border border-dark bg-dark bg-gradient " style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque illum impedit unde, aspernatur, minus omnis voluptatum temporibus.</p>
+  </div>
+</div>
+
+</template>
+
+<style scoped>
+</style>
